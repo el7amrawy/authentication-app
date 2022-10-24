a8c3aac023b40a2200dd1199e1cc00552d156e95
Aly <el7amrawy@outlook.com>
2022-10-25 01:45:44 +0200

    start on login page

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const config = {
   apiHost: import.meta.env.VITE_API,
   env: import.meta.env.VITE_ENV,
+  base: import.meta.env.VITE_ENV == "dev" ? "/" : "/authentication-app",
 };
 
 export default config;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,25 @@
-const Login = () => {
-  return <section></section>;
+import light_icon from "../assets/icons/devchallenges-light.svg";
+import darck_icon from "../assets/icons/devchallenges.svg";
+
+const Login = ({ theme }) => {
+  return (
+    <section className=" flex justify-center items-center w-full h-screen font-mono text-[#333333]">
+      <form className=" py-10 px-16 shadow-lg rounded-3xl">
+        {theme === "light" ? (
+          <img src={light_icon} alt="icon" />
+        ) : (
+          <img src={darck_icon} alt="icon" />
+        )}
+        <div className=" mt-8 text-lg font-semibold max-w-xs">
+          Join thousands of learners from around the world
+        </div>
+        <div className="max-w-xs mt-4">
+          Master web development by making real-life projects. There are
+          multiple paths for you to choose
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
